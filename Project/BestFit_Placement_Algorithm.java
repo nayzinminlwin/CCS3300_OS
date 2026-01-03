bf20e60a76f3e34fca54cf2d23079353d0ee5486
Nay Zin Min Lwin <nayzinminlwin22@gmail.com>
2026-01-04 02:07:29 +0800

    Finished Optimal Replacement Algorithm.

--- Project/BestFit_Placement_Algorithm.java
+++ Project/BestFit_Placement_Algorithm.java
@@ -4,12 +4,12 @@ public class BestFit_Placement_Algorithm {
 
     public static Object[][] BestFitAlgo(Object[][] currentMemory, int[] incomingPages) {
         Object[][] newMemory = currentMemory;
+        int lastRecentIndex = findLastRecentIndex(newMemory); // Find the last 'R' block index
 
         // loop through each incoming page
         for (int i = 0; i < incomingPages.length; i++) {
             int minIndex = -1; // To track the index of the best fit block
             int min = Integer.MAX_VALUE; // To track the size of the best fit block
-            int lastRecentIndex = findLastRecentIndex(newMemory); // Find the last 'R' block index
 
             // loop through memory blocks to find the best fit
             for (int j = 0; j < newMemory.length; j++) {
@@ -27,6 +27,13 @@ public class BestFit_Placement_Algorithm {
                         System.out
                                 .println("Perfect Fit for " + incomingPages[i] + "Kb Block Found at Index: " + j
                                         + " with Size: " + currentVal + "\n");
+
+                        // Update last recent index
+                        if (lastRecentIndex != -1) {
+                            newMemory[lastRecentIndex][0] = 'X';
+                        }
+                        lastRecentIndex = j;
+
                         minIndex = -2; // Indicate perfect fit found
                         break;
                     }
@@ -37,22 +44,36 @@ public class BestFit_Placement_Algorithm {
                     }
                 }
             }
+
+            // found the perfect fit
             if (minIndex == -2) {
-                newMemory[lastRecentIndex][0] = 'X';
-            } else if (minIndex == -1) {
+                // do nothing, already handled
+            }
+
+            // not found any suitable block
+            else if (minIndex == -1) {
                 System.out.println("No suitable block found for page size: " + incomingPages[i] + "\n");
-            } else {
+            }
+
+            // found the best fit
+            else {
                 System.out.println("Best Fit for " + incomingPages[i] + "Kb Block Found at Index: " + minIndex
                         + " with Size: " + min + "\n");
-                newMemory[lastRecentIndex][0] = 'X';
+                if (lastRecentIndex != -1) {
+                    newMemory[lastRecentIndex][0] = 'X';
+                }
+                lastRecentIndex = minIndex;
                 newMemory = resizeMemory(newMemory, minIndex, incomingPages[i]);
             }
+
+            // uncomment the following line to print memory state after each allocation
             // MemoryPlacement_Main.printFinalMemory(newMemory);
         }
 
         return newMemory;
     }
 
+    // Method to resize memory after allocation
     public static Object[][] resizeMemory(Object[][] memory, int index, int pageSize) {
         Object[][] updatedMemory = new Object[memory.length + 1][];
 
@@ -76,6 +97,7 @@ public class BestFit_Placement_Algorithm {
         return updatedMemory;
     }
 
+    // Method to find the most recent 'R' block index
     public static int findLastRecentIndex(Object[][] memory) {
         for (int i = memory.length - 1; i >= 0; i--) {
             if (memory[i][0].equals('R')) {
--- Project/Optimal_Replacement_Algorithm.java
+++ Project/Optimal_Replacement_Algorithm.java
@@ -1,5 +1,84 @@
 package Project;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Optimal_Replacement_Algorithm {
 
+    public static void Optimal_Replacement_Algo(int[] pages, int frames) {
+        ArrayList<Integer> memorySpace = new ArrayList<>();
+        ArrayList<Integer> lastUsed = new ArrayList<>(); // Track last used times
+        int pageFaults = 0;
+
+        for (int i = 0; i < pages.length; i++) {
+            // Check if page already exists in memory
+            if (!memorySpace.contains(pages[i])) {
+                pageFaults++;
+
+                if (memorySpace.size() < frames) {
+                    // Memory has space, just add the page
+                    memorySpace.add(pages[i]);
+                    lastUsed.add(i);
+                } else {
+                    int remainingPages = pages.length - (i + 1);
+                    int pageToReplacePos = -1;
+
+                    // If we have enough future pages to check all frames
+                    if (remainingPages >= frames) {
+                        // Use Optimal algorithm
+                        int farthestIndex = -1;
+
+                        for (int j = 0; j < memorySpace.size(); j++) {
+                            int nextUse = findNextOccurrence(i + 1, pages, memorySpace.get(j));
+
+                            if (nextUse == -1) {
+                                pageToReplacePos = j;
+                                break;
+                            }
+
+                            if (nextUse > farthestIndex) {
+                                farthestIndex = nextUse;
+                                pageToReplacePos = j;
+                            }
+                        }
+                    } else {
+                        // Not enough future references, use LRU
+                        System.out.println("Using LRU fallback (not enough future references)");
+                        int lruTime = Integer.MAX_VALUE;
+
+                        for (int j = 0; j < memorySpace.size(); j++) {
+                            if (lastUsed.get(j) < lruTime) {
+                                lruTime = lastUsed.get(j);
+                                pageToReplacePos = j;
+                            }
+                        }
+                    }
+
+                    memorySpace.set(pageToReplacePos, pages[i]);
+                    lastUsed.set(pageToReplacePos, i);
+                }
+
+                System.out.println(
+                        "Page fault! After inserting page " + pages[i] + ": " + Arrays.toString(memorySpace.toArray()));
+            } else {
+                // Update last used time for existing page
+                int index = memorySpace.indexOf(pages[i]);
+                lastUsed.set(index, i);
+
+                System.out
+                        .println("Page " + pages[i] + " already in memory: " + Arrays.toString(memorySpace.toArray()));
+            }
+        }
+
+        System.out.println("\nTotal Page Faults: " + pageFaults);
+    }
+
+    public static int findNextOccurrence(int startIndex, int[] pages, int searchVal) {
+        for (int i = startIndex; i < pages.length; i++) {
+            if (pages[i] == searchVal) {
+                return i;
+            }
+        }
+        return -1; // Not found in future
+    }
 }
